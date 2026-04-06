48a9c85d50e911b4f06359f104196c79dd059691
albert977-ctrl <albert977@gmail.com>
2026-04-05 20:15:03 -0400

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Uncomment to enable experimental features if needed
+  // experimental: {
+  //   esmExternals: true,
+  // },
+};
+
+export default nextConfig;
